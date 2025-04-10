d4ce1dae0b6dc62ed1dbad04b7fd3826845cac3e
Obasohan2 <jtp7d03u@students.codeinstitute.net>
2025-04-10 13:15:27 +0100

    my-project-hello-wrld

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-s^*-p((ugplay(v*lew-lrnw8k%mq5ltlin!656(#*8i4poyx9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
